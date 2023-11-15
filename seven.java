d866815b2e8fc3c9c4b5d4de5b9e94b093d9f9e1
Prosenjit_Mondol <prosenjit1156@gmail.com>
2023-11-15 11:22:45 +0600

    11/15/23

---
+++ seven.java
@@ -0,0 +1,14 @@
+class point{
+    private int x;
+    private int y;
+
+    public void setpoint(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public int get
+}
+public class seven {
+    
+}
