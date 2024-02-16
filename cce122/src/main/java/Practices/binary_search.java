2a6956019f604d95ea10b4e9eefd09a7956b94f6
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-16 23:06:42 +0600

    2/15/24

--- cce122/src/main/java/Practices/binary_search.java
+++ cce122/src/main/java/Practices/binary_search.java
@@ -4,10 +4,28 @@
  */
 package Practices;
 
-/**
- *
- * @author HP
- */
+
+
 public class binary_search {
-    
+    static void binarySearch(int arr[],int f,int l,int key){
+        int m=(f+l)/2;
+        while(f<=l){
+            if (arr[m]>key) {
+                l=m-1;
+            }
+            else if(arr[m]<key){
+                f=m+1;
+            }
+            else if(arr[m]==key){
+                System.out.println("Element found at index :"+m);
+                break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {10,20,30,40,50};  
+        int key = 30;  
+        int last=arr.length-1;  
+        binarySearch(arr,0,last,key);
+    }
 }
