956b26df7b19ead5f8afd27b40479790e89c1b3b
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-17 02:07:22 +0600

    2/17/24

---
+++ cce122/src/main/java/Practices/CurrentDateTime.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Practices;
+import java.util.Date;
+public class CurrentDateTime {
+    public static void main(String[] args) {
+        // Create a Date object representing the current date and time
+        Date currentDate = new Date();
+
+        // Print the current date and time
+        System.out.println("Current Date and Time: " + currentDate);
+    }
+}
