7615112c9d057ec1d3053aefdaa1a239d5b8b11f
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-17 00:29:49 +0600

    2/17/24

--- cce122/src/main/java/Practices/pascal.java
+++ cce122/src/main/java/Practices/pascal.java
@@ -26,7 +26,7 @@ public class pascal {
             System.out.print("\n");
         }
         for (int i =n-2; i>=0; i--) {
-            for (int j =0; j>=n-i; j--) {
+            for (int j =n-1; j>=i; j--) {
                 System.out.print(" ");
             }
             for (int j = 0; j <=i; j++) {
