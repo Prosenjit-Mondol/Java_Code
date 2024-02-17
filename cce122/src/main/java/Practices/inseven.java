42b6104dffd46cad0ef17f2d251a44c8daa94100
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-18 00:37:55 +0600

    2/18/24

--- cce122/src/main/java/Practices/inseven.java
+++ cce122/src/main/java/Practices/inseven.java
@@ -4,10 +4,59 @@
  */
 package Practices;
 
-/**
- *
- * @author HP
- */
+class point{
+    private int x;
+    private int y;
+
+    public point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+class quardrilateral{
+    private Point point1;
+    private Point point2;
+    private Point point3;
+    private Point point4;
+
+    public quardrilateral(Point point1, Point point2, Point point3, Point point4) {
+        this.point1 = point1;
+        this.point2 = point2;
+        this.point3 = point3;
+        this.point4 = point4;
+    }
+
+    public Point getPoint1() {
+        return point1;
+    }
+
+    public Point getPoint2() {
+        return point2;
+    }
+
+    public Point getPoint3() {
+        return point3;
+    }
+
+    public Point getPoint4() {
+        return point4;
+    }
+    public double A(){
+        return (double) Math.sqrt(Math.pow(getPoint2().getX() - getPoint1().getX(), 2)+ Math.pow(getPoint2().getY() - getPoint1().getY(), 2));
+    }
+    public double B() {
+   return (double) Math.sqrt(Math.pow(getPoint3().getX() - getPoint2().getX(), 2)+ Math.pow(getPoint3().getY() - getPoint2().getY(), 2));
+    }
+    
+}
 public class inseven {
     
 }
