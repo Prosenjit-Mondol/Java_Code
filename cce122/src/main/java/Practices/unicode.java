737803ed429758789e7f81fc82c59fa1474d2c93
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-17 18:01:02 +0600

    2/17/24

--- cce122/src/main/java/Practices/unicode.java
+++ cce122/src/main/java/Practices/unicode.java
@@ -4,10 +4,6 @@
  */
 package Practices;
 
-/**
- *
- * @author HP
- */
 public class unicode {
     public static void main(String[] args) {
         String str="Prosenjit";
@@ -15,5 +11,9 @@ public class unicode {
         int v1=str.codePointAt(0);
         int v2=str.codePointAt(8);
         System.out.println(v1+" "+v2);
+        int c=str.codePointCount(0, 5);
+        System.out.println("CodePoint count "+c);
+        int index=str.indexOf("i",0);
+        System.out.println("Index of i is "+index);
     }
 }
